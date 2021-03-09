d1afac34db1749802ec449c71e5d67afe4140e33
Trejon-Hughes <trejonhughes15@gmail.com>
2021-03-09 14:58:57 -0500

    added value types

---
+++ ValueType/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ValueType")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ValueType")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18ea7fe8-6dfe-45c8-a1ed-5c24336d9ae0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ValueType/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ValueType
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Booleans()
+        {
+            bool isDeclared;
+            isDeclared = true;
+
+            bool isDeclaredAndInitialized = false;
+        }
+        [TestMethod]
+        public void Characters()
+        {
+            char character = 'a';
+            char symbol = '?';
+            char numbers = '8';
+            char space = ' ';
+            char special = '\n';
+        }
+        [TestMethod]
+        public void WholeNumbers()
+        {
+            byte byteExample = 255;
+            sbyte sByteExample = 127;
+            short shortExample = 32767;
+            Int16 anotherShortExample = 32000;
+            int intMin = 2147483647;
+            Int32 intMax = 2147483647;
+            long longExample = 9223372036854775807;
+            Int64 longMin= -9223372036854775807;
+
+            int a = 15;
+            int b = -25;
+
+            byte age = 25;
+        }
+        [TestMethod]
+        public void Decimals()
+        {
+            float floatExample = 1.045231f;
+            double doubleExample = 1.5353536343434d;
+            decimal decimalExample = 1.253536353253253535523535m;
+        }
+        enum PastryType { Cake, Cupcake, Eclaire, Petitfour, Croissant}
+        [TestMethod]
+        public void Enums()
+        {
+            PastryType myPastry = PastryType.Cake;
+            PastryType anotherOne = PastryType.Croissant;
+        }
+        [TestMethod]
+        public void Structs()
+        {
+            DateTime today = DateTime.Today;
+
+            DateTime birthday = new DateTime(1800, 6, 3);
+        }
+    }
+}
