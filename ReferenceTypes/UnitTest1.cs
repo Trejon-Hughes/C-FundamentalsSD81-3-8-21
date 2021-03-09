0713ce3b91811c78d3be4febe720421798d916e5
Trejon-Hughes <trejonhughes15@gmail.com>
2021-03-09 14:01:29 -0500

    Add project files.

---
+++ ReferenceTypes/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ReferenceTypes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Strings()
+        {
+            string name;
+
+            string declared;
+            declared = "this is initialized.";
+
+            string declareAndInitialized = "This is bothdeclaring and initializing.";
+
+            string firstName = "Adam";
+            string lastName = "Wolain";
+
+            string concatenatedFullName = firstName + " " + lastName;
+
+            string interpolationFullName = $"{firstName} {lastName}";
+
+            string compositeFullName = string.Format("{0} {1}", firstName, lastName);
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(compositeFullName);
+            Console.WriteLine(interpolationFullName);
+            Console.WriteLine(compositeFullName);
+
+        }
+        [TestMethod]
+        public void Collections()
+        {
+            string stringExample = "Hello World";
+
+            string[] stringArray = { "Hello", "World!", "Why", "is it", "always", stringExample };
+
+            string thirdItem = stringArray[2];
+            Console.WriteLine(thirdItem);
+
+            stringArray[0] = "Hey There";
+            Console.WriteLine(stringArray[0]);
+
+            List<string> listOfStrings = new List<string>();
+            List<int> listOfInts = new List<int>();
+
+            listOfStrings.Add("First string for our list");
+            listOfInts.Add(134433);
+
+            Console.WriteLine(listOfInts[0]);
+
+            Queue<string> firstInFirstOut = new Queue<string>();
+
+            firstInFirstOut.Enqueue("I'm First");
+            firstInFirstOut.Enqueue("I'm Next");
+
+            string firstItem = firstInFirstOut.Dequeue();
+            Console.WriteLine(firstItem);
+
+            Dictionary<int, string> keyAndValue = new Dictionary<int, string>();
+
+            keyAndValue.Add(7, "Agent");
+
+            string valueSeven = keyAndValue[7];
+            Console.WriteLine(valueSeven);
+        }
+        [TestMethod]
+        public void Classes()
+        {
+            Random rng = new Random();
+
+            int randomNumber = rng.Next();
+
+            Console.WriteLine(randomNumber);
+        }
+    }
+}
